b46acb7c6a09df8f5b495ae74f143c899d0bf444
Aline Cristina Franco Ferreira <alinecff.89@gmail.com>
2022-08-23 17:34:06 -0300

    created components

--- apicomidavue/src/App.vue
+++ apicomidavue/src/App.vue
@@ -11,12 +11,31 @@ import HelloWorld from './components/HelloWorld.vue'
     <!-- use the router-link component for navigation. -->
     <!-- specify the link by passing the `to` prop. -->
     <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/login">Login</router-link>
-    <router-link to="/sign">Sigg in</router-link>
+    <router-link to="/login">Home</router-link>
+    <router-link to="/sign">Logar</router-link>
+    <router-link to="/sign">Cadastrar</router-link>
+    <router-link to="/sign">Contato</router-link>
   </nav>
   <!-- route outlet -->
   <!-- component matched by the route will render here -->
   <router-view></router-view>
+</div>
+
+
+
+
+<div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">Home</b-nav-item>
+
+      <!-- Navbar dropdowns -->
+     
+
+      
+    </b-navbar-nav>
+  </b-navbar>
 </div>
 </template>
 
